70f89e5f108ac41b6ad3ae6045c71e01e0d8d3b4
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2025-09-18 12:41:11 +0600

    Problem 26: A student has to submit 3 assignments before 10 pm and he starts to do the assignments at X pm. Each assignment takes him 1 hour to complete. Can you tell whether he'll be able to complete all assignments on time or not?

---
+++ Assignment26.java
@@ -0,0 +1,10 @@
+// Problem: Assignment Deadline
+import java.util.Scanner;
+public class Assignment26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if (X + 3 <= 10) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
