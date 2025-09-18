e6c33abb1f36eebbb598269424b266418a0bc193
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2025-09-18 10:56:40 +0600

    Changed the file name of Greetuser program

--- GreetUser02.java
+++ GreetUser02.java
@@ -1,6 +1,6 @@
 // Problem: Greet User
 import java.util.Scanner;
-public class GreetUser01 {
+public class GreetUser02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
