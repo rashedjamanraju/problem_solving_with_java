eb3ef5459247bb117ffbf67e2f148de87040d481
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2025-09-18 12:46:46 +0600

    Problem 28: Recently You joined a new company. In this company, the employees have to work for X hours each day from Saturday to Wednesday. Also, in this company, Wednesday is called Chill Day, employees only have to work for Y hours (Y<X) on Wednesday.
    Determine the total number of working hours in one week.

---
+++ WorkHour28.java
@@ -0,0 +1,10 @@
+// Problem: Work Hour
+import java.util.Scanner;
+public class WorkHour28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt(), Y = sc.nextInt();
+        int total = X * 4 + Y;
+        System.out.println(total);
+    }
+}
