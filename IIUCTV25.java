c2d83f907c8367368f532e52376444aaebe8c525
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2025-09-18 12:35:50 +0600

    Problem 25: IIUCTV

--- IIUCTV25.java
+++ IIUCTV25.java
@@ -4,7 +4,6 @@ public class IIUCTV25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(), X = sc.nextInt();
-        int subs = (N + 5) / 6;
         if(N%6==0){
             System.out.println((N/6)*X);
         }
