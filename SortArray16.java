1f8409b184e0c307ae5c6a26f081d98a9971c032
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2025-09-18 11:25:39 +0600

    Problem 16: Sort array

---
+++ SortArray16.java
@@ -0,0 +1,13 @@
+// Problem: Sort Array Ascending
+import java.util.Scanner;
+import java.util.Arrays;
+public class SortArray16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        for (int x : arr) System.out.print(x + " ");
+    }
+}
