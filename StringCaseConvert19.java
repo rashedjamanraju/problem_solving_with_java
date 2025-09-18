7214c55f9b8dcda58dff1394f5646922c1ed34dc
rashedjamanraju <c251100@ugrad.iiuc.ac.bd>
2025-09-18 11:31:11 +0600

    Problem 19: case converter

---
+++ StringCaseConvert19.java
@@ -0,0 +1,10 @@
+// Problem: Convert String Case
+import java.util.Scanner;
+public class StringCaseConvert19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println("Uppercase: " + s.toUpperCase());
+        System.out.println("Lowercase: " + s.toLowerCase());
+    }
+}
